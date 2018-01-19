da4c3945af1d1903acaacb3b595218230d8ab5af
Jeremy L <jeremylee.business@gmail.com>
2018-01-18 16:01:00 -0800

    Revert "Added hyperlinks to images"

--- application/views/homepage.php
+++ application/views/homepage.php
@@ -1,7 +1,5 @@
 <div class="row">
-        <div class="span4">
-                <a href="/welcome/show/{key}">
-                        <img src="/pix/{who}.jpg" title="{who} plays {role}"/>
-                </a>
-        </div>
+    {authors}
+    <div class="span4"><img src="/pix/{who}.jpg" title="{who} plays {role}"/></div>
+    {/authors}
 </div>
